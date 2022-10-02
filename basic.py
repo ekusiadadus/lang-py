e38c662468991ad09f7c0176cc1ab7e692572a7a
ekusiadadus <ekusiadadus@gmail.com>
2022-10-03 02:58:05 +0900

    feat: add Position

--- basic.py
+++ basic.py
@@ -8,17 +8,46 @@ DIGITS = '0123456789'
 #
 
 class Error:
-  def __init__(self, error_name, details):
+  def __init__(self, pos_start, pos_end, error_name, details):
+    self.pos_start = pos_start
+    self.pos_end = pos_end
     self.error_name = error_name
     self.details = details
 
   def as_string(self):
     result = f'{self.error_name}: {self.details}'
+    result = f'File {self.pos_start.fn}, line {self.pos_start.line + 1}'
     return result
 
 class IllegalCharError(Error):
-  def __init__(self, details):
-    super().__init__('Illegal Character', details)
+  def __init__(self, pos_start, pos_end, details):
+    super().__init__(pos_start, pos_end,'Illegal Character', details)
+
+
+#
+# POSITION
+#
+
+class Position:
+  def __init__(self, index, line, column, fn , ftxt):
+    self.index = index
+    self.line = line
+    self.column = column
+    self.fn = fn
+    self.ftxt = ftxt
+
+  def advance(self, current_char=None):
+    self.index = self.index + 1
+    self.column = self.column + 1
+
+    if current_char == '\n':
+      self.index += 1
+      self.column = 0
+
+    return self
+
+  def copy(self):
+    return Position(self.index, self.line, self.column, self.fn, self.ftxt)
 
 #
 # declare type of variable
@@ -44,15 +73,16 @@ class Token:
     return f'{self.type}'
 
 class Lexer:
-  def __init__(self, text):
+  def __init__(self, fn, text):
+    self.fn = fn
     self.text = text
-    self.pos = -1
+    self.pos = Position(-1, 0, -1, fn, text)
     self.current_char = None
     self.advance()
 
   def advance(self):
-    self.pos += 1
-    self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+    self.pos.advance(self.current_char)
+    self.current_char = self.text[self.pos.index] if self.pos.index < len(self.text) else None
 
   def make_tokens(self):
     tokens = []
@@ -80,9 +110,10 @@ class Lexer:
         tokens.append(Token(TT_RPAREN))
         self.advance()
       else:
+        pos_start = self.pos.copy()
         char = self.current_char
         self.advance()
-        return [], IllegalCharError("'" + char + "'")
+        return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
     return tokens, None
 
   def make_number(self):
@@ -105,7 +136,7 @@ class Lexer:
 # RUN
 #
 
-def run(text):
-  lexer = Lexer(text)
+def run(fn, text):
+  lexer = Lexer(fn, text)
   tokens, error = lexer.make_tokens()
   return tokens, error
